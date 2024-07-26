f0f93d63deaf4b54effb2252b62fdcf9e3b71aaa
souvik <souvikk431@gmail.com>
2024-07-26 19:17:18 +0530

    checking husky

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@repo/ui/components/ui/button";
 export default function Page() {
   return (
     <main className="min-w-screen h-[100dvh] grid place-items-center">
-      <Button>Click me</Button>
+      <Button>Click me, i am a button</Button>
     </main>
   );
 }
--- apps/web/lib/config.ts
+++ apps/web/lib/config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable turbo/no-undeclared-env-vars */
 export const config = {
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET ?? "",
   NEXTAUTH_URL: process.env.NEXTAUTH_URL ?? "",
